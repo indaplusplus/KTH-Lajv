566238e0a41780de4731d4d8d7f767d0f9f70808
vinell <vinell@kth.se>
2018-05-09 21:02:08 +0200

    Commenting and getting comments works.

--- comments/Comment.go
+++ comments/Comment.go
@@ -6,42 +6,54 @@ import (
 	"net/http"
 )
 
-var database string = "http://127.0.0.1:219"
+var database string = "http://127.0.0.1:55994"
 
 func post(w http.ResponseWriter, r *http.Request, data jsonData) {
+	data.Command = "comment"
 	data.Text = filter(data.Text)
 	jsonVal, err := json.Marshal(data)
 
-	if err == nil {
-		http.Post(database+"/comment", "application/json", bytes.NewBuffer(jsonVal))
+	if err != nil {
+		return
 	}
+
+	http.Post(database, "application/json", bytes.NewBuffer(jsonVal))
 }
 
 func like(w http.ResponseWriter, r *http.Request, data jsonData) {
+	data.Command = "upvote-comment"
 	jsonVal, err := json.Marshal(data)
 
-	if err == nil {
-		http.Post(database+"/upvote-comment", "application/json", bytes.NewBuffer(jsonVal))
+	if err != nil {
+		return
 	}
+
+	http.Post(database, "application/json", bytes.NewBuffer(jsonVal))
 }
 
 func delete(w http.ResponseWriter, r *http.Request, data jsonData) {
+	data.Command = "delete-comment"
 	jsonVal, err := json.Marshal(data)
 
-	if err == nil {
-		http.Post(database+"/delete-comment", "application/json", bytes.NewBuffer(jsonVal))
+	if err != nil {
+		return
 	}
+
+	http.Post(database, "application/json", bytes.NewBuffer(jsonVal))
 }
 
 func get(w http.ResponseWriter, r *http.Request, data jsonData) {
+	data.Command = "get-comments"
 	jsonVal, err1 := json.Marshal(data)
 
-	if err1 == nil {
-		resp, err2 := http.Post(database+"/get-comments", "application/json", bytes.NewBuffer(jsonVal))
-
-		var dbData jsonData
-		json.NewDecoder(resp.Body).Decode(&dbData) //decode database response
-
-		json.NewEncoder(w).Encode(dbData) //now write out the same shit
+	if err1 != nil {
+		return
 	}
+
+	resp, _ := http.Post(database, "application/json", bytes.NewBuffer(jsonVal))
+
+	var dbData jsonData
+	json.NewDecoder(resp.Body).Decode(&dbData)
+
+	json.NewEncoder(w).Encode(dbData.Comments)
 }
